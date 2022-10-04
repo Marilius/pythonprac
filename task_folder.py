f28580a01119cd96c7dc70a1468954e5b1231c16
Marilius <38788888+Marilius@users.noreply.github.com>
2022-10-04 11:26:45 +0300

    инициализтор папки с решениями

---
+++ task_folder.py
@@ -0,0 +1,26 @@
+import datetime 
+import os
+
+currentDate = datetime.date.today()
+
+data = currentDate.strftime("%Y%m%d")
+isExist = os.path.exists(data)
+
+if not isExist:
+    os.makedirs(data)
+    print("The new directory is created!")
+
+for i in range(1, 4):
+    path = f'{data}/{i}'
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+    open(f'{path}/prog.py', 'a+')
+    path = f'{path}/tests'
+
+    if not isExist:
+        os.makedirs(path)
+
+    for j in range(1, 4):
+        open(f'{path}/{j}.in', 'a+')
+        open(f'{path}/{j}.out', 'a+')
