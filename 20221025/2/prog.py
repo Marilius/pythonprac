e5369680901d409a623103b7deb7a4b052b9a8c2
Marilius <38788888+Marilius@users.noreply.github.com>
2022-10-25 11:33:10 +0300

    2 задача

---
+++ 20221025/2/prog.py
@@ -0,0 +1,21 @@
+import sys
+from itertools import islice, tee
+
+
+def slide(seq, n):
+    while True:
+        s, seq = tee(seq)
+        s = list(s)
+        if not s:
+            return
+        yield from islice(s, n)
+        next(seq)
+
+
+print(*list(slide('help me plz', 1)))
+# print(*list(slide(range(5), 3)))
+# print(list(islice('abcdef', 10)))
+
+# exec(sys.stdin.read())
+
+
