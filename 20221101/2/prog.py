30a0fff1d5fe1abd5e9d6d7d7bae039246f378cf
Marilius <38788888+Marilius@users.noreply.github.com>
2022-11-07 21:42:57 +0300

    2

--- 20221101/2/prog.py
+++ 20221101/2/prog.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Triangle:
     def __init__(self, a, b, c):
         self.a, self.b, self.c = list(a), list(b), list(c)
@@ -14,16 +17,45 @@ class Triangle:
         else:
             return 0
 
+    def __bool__(self):
+        return bool(abs(self))
+
     def __lt__(self, other):
         return abs(self) < abs(other)
 
     def __gt__(self, other):
         return abs(self) > abs(other)
 
+    def __contains__(self, item):
+        if item.__class__ == Triangle:
+            if item:
+                return item.a in self and item.b in self and item.c in self
+            return True
+        else:
+            x0, y0 = self.a
+            x1, y1 = self.b
+            x2, y2 = self.c
+            x, y = item
+            a = (x0 - x) * (y1 - y0) - (x1 - x0) * (y0 - y)
+            b = (x1 - x) * (y2 - y1) - (x2 - x1) * (y1 - y)
+            c = (x2 - x) * (y0 - y2) - (x0 - x2) * (y2 - y)
+            return a * b >= 0 and b * c >= 0 and a * c >= 0
 
-test1 = Triangle((0, 0), (1, 1), (2, 0))
-test2 = Triangle((0, 0), (1, 2), (2, 0))
-print((abs(test1)))
-print((abs(test2)))
-print(test1 < test2)
-print(test1 > test2)
+    def __and__(self, other):
+        def g(other, self):
+            return not (other.a in self and other.b in self and other.c in self) and \
+                   (other.a in self or other.b in self or other.c in self)
+
+        return bool(self) and bool(other) and (g(self, other) or g(other, self))
+
+
+# r = Triangle((4, 2), (1, 0), (2, 5))
+# s = Triangle((1, 3), (3, 1), (2, 5))
+# t = Triangle((0, 0), (1, 3), (4, 5))
+# o = Triangle((1, 1), (2, 2), (3, 5))
+# print(*(f"{n}({bool(x)}):{round(abs(x), 3)}" for n, x in zip("rsto", (r, s, t, o))))
+# print(f"{s < t=}, {o < t=}, {r < t=}, {r < s=}")
+# print(f"{s in t=}, {o in t=}, {r in t=}")
+# print(f"{r & t=}, {t & r=}, {s & r=}, {o & t=}")
+
+exec(sys.stdin.read())
