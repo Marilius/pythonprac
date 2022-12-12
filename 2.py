9d1b84f72efff9c0c52260447e6dd0b7097470dd
Marilius <38788888+Marilius@users.noreply.github.com>
2022-12-12 23:30:15 +0300

    1

--- 2.py
+++ 2.py
@@ -1,55 +1,47 @@
-from numbers import Real
+from inspect import get_annotations, getfullargspec, formatargspec
+from types import GenericAlias
 from functools import wraps
 
-# class fixed(type):
-#     def __new__(cls, name, bases, namespace, ndigits=3):
-#         class_obj = super().__new__(cls, name, bases, namespace)
-#         method_list = [func for func in dir(class_obj) if callable(getattr(class_obj, func)) and not func.startswith('__')]
-#         for i in method_list:
-#             class_f = getattr(class_obj, i)
-# 
-#             def f(*args, **kwargs):
-#                 ans = class_f(*args)
-#                 if isinstance(ans, Real):
-#                     return round(ans, ndigits)
-#                 else:
-#                     return ans
-# 
-#             setattr(class_obj, i, f)
-# 
-#         return class_obj
+
+class init(type):
+    def __init__(cls, name, bases, namespace, *args, **kwargs):
+        print(get_annotations(cls))
+        print(dir(cls))
+        for name in dir(cls):
+            func = getattr(cls, name)
+            if callable(func):
+                print(func)
+                params = getfullargspec(func)
+                print(params)
+                # print(get_annotations(func))
+                # def
+                # @wraps(func)
+                # def f(*a, **b):
+                #     pass
+
+        for attr_name, annotation_type in get_annotations(cls).items():
+            print(1)
+            if not hasattr(cls, attr_name):
+                print('!!!')
+            else:
+                print(attr_name)
+                # attr = getattr(cls, attr_name)
+
+            # if not callable(attr):
+            #     attr_type = type(attr)
+            #     if type(annotation_type) == GenericAlias:
+            #         annotation_type = annotation_type.__origin__
+            #     if attr_type is not annotation_type and not issubclass(attr_type, annotation_type):
+            #         return False
+
+        # setattr(cls, data.__name__, data)
+        super().__init__(name, bases, namespace)
 
 
-class fixed(type):
-    def __new__(cls, name, bases, namespace, ndigits=3):
-        def f(fun):
-            @wraps(fun)
-            def wrapper(self, *a, **b):
-                ans = fun(self, *a, **b)
-                if isinstance(ans, Real):
-                    return round(ans, ndigits)
-                else:
-                    return ans
-
-            return wrapper
-
-        class_obj = super().__new__(cls, name, bases, namespace)
-
-        for key, value in namespace.items():
-            if callable(value):
-                setattr(class_obj, key, f(value))
-
-        return class_obj
+class C(metaclass=init):
+    def __init__(self, var: int, rng: range, lst: list[int], defined: str = "defined"):
+        self.data = f"{var}/{rng}/{lst}/{defined}"
 
 
-
-# from fractions import Fraction
-# from decimal import Decimal
-#
-# class C(metaclass=fixed, ndigits=4):
-#     def div(self, a, b):
-#         return a / b
-#
-# print(C().div(6, 7))
-# print(C().div(Fraction(6), Fraction(7)))
-# print(C().div(Decimal(6), Decimal(7)))
+for c in [C(var=1, rng=range(3), lst=[1], defined='sss')]:
+    print(c.data)
