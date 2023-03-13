81a8e23fab6501c9f05df16da299efc51a1cb560
Marilius <38788888+Marilius@users.noreply.github.com>
2023-03-13 15:58:23 +0300

    0

--- 20230313/0/client.py
+++ 20230313/0/client.py
@@ -4,5 +4,5 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((sys.argv[1], int(sys.argv[2]) if len(sys.argv) > 2 else 1337))
     while msg := sys.stdin.readline():
-        s.sendall(msg.strip().encode())
-        print(s.recv(1024).decode())
+        s.sendall(msg.encode())
+        print(s.recv(1024).decode().strip())
